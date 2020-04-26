d89b5f368f4fe969e0606623ec5f0e5b81c99b3d
Sai Koka <ssk177@scarletmail.rutgers.edu>
2020-04-25 23:26:18 -0400

    XD

--- ChessAndroid72/app/src/main/java/model/Square.java
+++ ChessAndroid72/app/src/main/java/model/Square.java
@@ -1,11 +1,4 @@
 package model;
-import chess.Piece;
+
 public class Square {
-    private int row;
-    private int column;
-    Piece piece;
-    public Square(int row, int column, Piece piece){
-
-    }
-
 }
