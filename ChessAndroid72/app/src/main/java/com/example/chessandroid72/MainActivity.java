29f6bb27a469575e9b853d34c1d0ca626825e90c
Sai Koka <ssk177@scarletmail.rutgers.edu>
2020-04-25 23:11:01 -0400

    Initial test

--- ChessAndroid72/app/src/main/java/com/example/chessandroid72/MainActivity.java
+++ ChessAndroid72/app/src/main/java/com/example/chessandroid72/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.chessandroid72;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
-
+    GridView board;
+    int[] squares = {R.drawable.blacksquare, R.drawable.whitesquare};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        board = findViewById(R.id.chessboard);
+        BoardAdapter customAdapter = new BoardAdapter();
     }
+
+    /*private class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view1 = getLayoutInflater().inflate(R.layout.row_data, null);
+
+            return null;
+        }
+    }*/
 }
---
+++ ChessAndroid72/app/src/main/java/com/example/chessandroid72/Square.java
@@ -0,0 +1,6 @@
+package com.example.chessandroid72;
+
+public class Square {
+
+    //
+}
